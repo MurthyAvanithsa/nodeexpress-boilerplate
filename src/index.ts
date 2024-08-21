bc7de816b42019e1f8764e265ad5cc8018f25e33
Deepika2430 <deepika_pabbisetti@tecnics.com>
2024-08-21 18:04:10 +0530

    Updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { Command } from 'commander';
 import { startServer } from './app';  
+import 'dotenv/config'
 
 const program = new Command();
 
 program
   .command('server')
-  .option('-p, --port <number>', 'port to listen on', "3000")  
+  .option('-p, --port <number>', 'port to listen on', process.env.port || "3000")  
   .action((cmd) => {
     startServer(cmd);
   });
