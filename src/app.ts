95e822d89450f325a39b7ab4da4d41b8207df127
Deepika Pabbisetti <deepika_pabbisetti@tecnics.com>
2024-08-22 13:09:00 +0530

    Add swagger ui
    Added route api-docs to include swagger ui

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import { logger } from './logger/log';
 import 'dotenv/config';
 import feedRouter from './routes/feed.routes';
 import filterRouter from './routes/filter.routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 
 const app = express();
 app.use(function (req, res, next) {
@@ -11,6 +13,7 @@ app.use(function (req, res, next) {
 })
 app.use(feedRouter);
 app.use(filterRouter)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export function startServer(options: any) {
   app.listen(options.port, () => {
