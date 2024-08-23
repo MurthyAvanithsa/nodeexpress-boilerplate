c5e148f47262ffa571a2e658cd8364ad30ce9cbb
Abhi Kumar Yallapu <abhi_yallapu@tecnics.com>
2024-08-23 12:29:44 +0530

    Added request and error logging middleware
    - Implemented `requestLogger` to log HTTP method and path for incoming requests.
    - Implemented `errorLogger` to log validation and unexpected errors, sending appropriate HTTP responses.
    - Integrated with `pino` logger for consistent logging across the application.

---
+++ src/middleware/logger.middleware.ts
@@ -0,0 +1,21 @@
+import { logger } from '../logger/log';
+import { Request, Response, NextFunction } from 'express';
+
+export const errorLogger = (err: any, req: Request, res: Response, next: NextFunction) => {
+    if (err.errors) {
+        logger.error(`Validation error: ${JSON.stringify(err.errors, null, 2)}`);
+        res.status(400).json({
+            error: err.errors,
+        });
+    } else {
+        logger.error(`Unexpected error: ${err.message}`);
+        res.status(500).json({
+            error: 'An unexpected error occurred',
+        });
+    }
+}
+
+export const requestLogger = (req: Request, res: Response, next: NextFunction) => {
+    logger.info(`${req.method} ${req.path}`);
+    next();
+}
