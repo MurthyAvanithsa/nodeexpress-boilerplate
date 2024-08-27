ea967b269764aec7e9b1e214c497918c40fd1e13
Deepika Pabbisetti <deepika_pabbisetti@tecnics.com>
2024-08-27 17:45:09 +0530

    Add worker command

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Command } from 'commander';
 import { startServer } from './app';  
+import { startWorker } from './workers/ad-breaks';
 import 'dotenv/config'
 
 const program = new Command();
@@ -9,6 +10,13 @@ program
   .option('-p, --port <number>', 'port to listen on', process.env.port || "3000")  
   .action((cmd) => {
     startServer(cmd);
+  });
+
+program
+  .command('worker')
+  .option('-q, --queue <string>', 'queue name')  
+  .action((cmd) => {
+    startWorker(cmd.queue);
   });
 
 program.parse(process.argv);
