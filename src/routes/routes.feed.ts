6ed2bd06c2b97d3c90fd8211d7ece0effe426d54
Deepika Pabbisetti <deepika_pabbisetti@tecnics.com>
2024-08-28 17:12:45 +0530

    Update feed routes

--- src/routes/routes.feed.ts
+++ src/routes/routes.feed.ts
@@ -1,41 +1,54 @@
 import { Router, Request, Response } from 'express';
 import {
-    Feed,
     GetFeedsResponse,
     PostFeedResponse,
     GetFeedResponse,
     UpdateFeedResponse,
     DeleteFeedResponse,
     PostFeedRequestBody,
-    UpdateFeedRequestBody
+    UpdateFeedRequestBody,
+    GetFeedByIdRequest,
+    DeleteFeedRequest
 } from '../types/types.feed';
 import { getFeeds, createFeed, getFeedById, updateFeed, deleteFeed } from '../services/services.feed';
 
 const feedRouter = Router();
 
 feedRouter.get('/feed', async (req: Request, res: Response<GetFeedsResponse>) => {
-    const responseData: GetFeedsResponse = { data: await getFeeds() };
-    res.send(responseData);
+    const response: GetFeedsResponse = await getFeeds();
+    const responseCode = response.error ? 500 : 200;
+    res.status(responseCode).json(response);
 });
 
 feedRouter.post('/feed', async (req: Request<{}, PostFeedResponse, PostFeedRequestBody>, res: Response<PostFeedResponse>) => {
-    const responseData: PostFeedResponse = { data: await createFeed(req.body) };
-    res.send(responseData);
+    const feed: PostFeedRequestBody = req.body;
+    const response: PostFeedResponse = await createFeed(feed);
+    const responseCode = response.data ? 201: 500;
+    res.status(responseCode).json(response);
 });
 
-feedRouter.get('/feed/:feedId', async (req: Request<{ feedId: Pick<Feed, 'id'> }>, res: Response<GetFeedResponse>) => {
-    const responseData: GetFeedResponse = { data: await getFeedById(req.params.feedId) };
-    res.send(responseData);
+feedRouter.get('/feed/:id', async (req: Request<GetFeedByIdRequest>, res: Response<GetFeedResponse>) => {
+    const feedId = req.params.id;
+    const response: GetFeedResponse = await getFeedById(feedId);
+    let responseCode;
+    response.data? responseCode = 200: response.error=="Filter not found"? responseCode=404: responseCode = 500; 
+    res.status(responseCode).json(response); 
 });
 
-feedRouter.put('/feed/:feedId', async (req: Request<{ feedId: Pick<Feed, 'id'> }, UpdateFeedResponse, UpdateFeedRequestBody>, res: Response<UpdateFeedResponse>) => {
-    const responseData: UpdateFeedResponse = { data: await updateFeed(req.params.feedId, req.body) };
-    res.send(responseData);                          
+feedRouter.put('/feed/:id', async (req: Request<GetFeedByIdRequest, UpdateFeedResponse, UpdateFeedRequestBody>, res: Response<UpdateFeedResponse>) => {
+    const feedId = req.params.id;
+    const feed: UpdateFeedRequestBody = req.body;
+    const response: UpdateFeedResponse = await updateFeed(feedId, feed);
+    const responseCode = response.data? 200 : 500;
+    res.status(responseCode).json(response);
 });
 
-feedRouter.delete('/feed/:feedId', async (req: Request<{ feedId: Pick<Feed, 'id'>}, DeleteFeedResponse, {}>, res: Response<DeleteFeedResponse>) => {
-    const responseData: DeleteFeedResponse = { data: await deleteFeed(req.params.feedId) };
-    res.send(responseData);
+feedRouter.delete('/feed/:id', async (req: Request<DeleteFeedRequest, DeleteFeedResponse, {}>, res: Response<DeleteFeedResponse>) => {
+    const feedId = req.params.id;
+    const response: DeleteFeedResponse = await deleteFeed(feedId);
+    let responseCode;
+    response.data? responseCode = 204: responseCode = 500;
+    res.status(responseCode).json(response);
 });
 
 export default feedRouter;
