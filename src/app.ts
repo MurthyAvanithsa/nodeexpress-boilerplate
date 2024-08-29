4c8f7291e7d91754bfbe01e5648b6af30f85c57e
Deepika Pabbisetti <deepika_pabbisetti@tecnics.com>
2024-08-29 17:58:23 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -19,8 +19,8 @@ import { jwtMiddleware } from "./middleware/jwt-authorization";
 
 const adBreaksQueue = new Queue('adBreaksQueue', {
   connection: {
-    host: 'localhost',
-    port: 6379,
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
   },
 });
 
