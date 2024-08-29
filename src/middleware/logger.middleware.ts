4df585991ade1da9fbe73af99108bc07f8369f7d
Rajesh Kandregula <rajesh_kandregula@tecnics.com>
2024-08-29 16:03:49 +0530

    Update logger.middleware.ts
    Add Unauthorized error handling and response
    
    - Implemented condition to check for Unauthorized errors
    - Added response for Unauthorized access

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,21 +1,33 @@
-import { logger } from '../logger/log';
-import { Request, Response, NextFunction } from 'express';
+import { logger } from "../logger/log";
+import { Request, Response, NextFunction } from "express";
 
-export const errorLogger = (err: any, req: Request, res: Response, next: NextFunction) => {
-    if (err.errors) {
-        logger.error(`Validation error: ${JSON.stringify(err.errors, null, 2)}`);
-        res.status(400).json({
-            error: err.errors,
-        });
-    } else {
-        logger.error(`Unexpected error: ${err.message}`);
-        res.status(500).json({
-            error: `An unexpected error occurred: ${err.message}`,
-        });
-    }
-}
+export const errorLogger = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err.name === "UnauthorizedError") {
+    logger.error(`Unauthorized error: ${err.message}`);
+    return res.status(401).json({ message: "Invalid or missing token" });
+  } else if (err.errors) {
+    logger.error(`Validation error: ${JSON.stringify(err.errors, null, 2)}`);
+    res.status(400).json({
+      error: err.errors,
+    });
+  } else {
+    logger.error(`Unexpected error: ${err.message}`);
+    res.status(500).json({
+      error: `An unexpected error occurred: ${err.message}`,
+    });
+  }
+};
 
-export const requestLogger = (req: Request, res: Response, next: NextFunction) => {
-    logger.info(`${req.method} ${req.path}`);
-    next();
-}
+export const requestLogger = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  logger.info(`${req.method} ${req.path}`);
+  next();
+};
