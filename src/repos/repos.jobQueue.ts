cf31efcca0f2420a6c16e2abf1d79e4ec26a89f7
Deepika Pabbisetti <deepika_pabbisetti@tecnics.com>
2024-09-02 11:21:34 +0530

    Add db functions to add and update a job
    Create repos.jobQueue.ts
    Add functions to add a job to the database when a new job is created and to update a job when worker executes the job.

---
+++ src/repos/repos.jobQueue.ts
@@ -0,0 +1,31 @@
+import { CloudEvent } from "cloudevents";
+import { prismaConnection } from "../connections";
+import { JobQueue as jobQueueModel } from "@prisma/client"
+import { logger } from "../logger/log";
+
+async function createJobQueue(queueName: string, id: string, payload: CloudEvent): Promise<jobQueueModel> {
+    const insertedJob: jobQueueModel = await prismaConnection.jobQueue.create({
+        data: {
+            queueName: queueName,
+            status: "Waiting",
+            jobId: id,
+            payload: payload,
+            createdAt: payload.time
+        }
+    });
+    return insertedJob;
+}
+
+async function updateJobQueue(queueName: string, id: string, updates: { status: string, completedAt?: string, error?: string }) {
+    const updatedJob = await prismaConnection.jobQueue.update({
+        where: {
+            queueName_jobId: {
+                queueName: queueName,
+                jobId: id
+            }
+        },
+        data: updates
+    });
+    return updatedJob;
+}
+export { createJobQueue, updateJobQueue }
