a7835a32ba8d5fc7f14492f534f0c9143d18fbf2
Deepika Pabbisetti <deepika_pabbisetti@tecnics.com>
2024-09-02 11:29:03 +0530

    Implement addJob()

--- src/workers/generic.ts
+++ src/workers/generic.ts
@@ -1,47 +1,57 @@
 import { Worker, Job, Queue } from "bullmq";
-import config from "../config";
 import { redisConnection } from "../connections";
-
-
+import { logger } from "../logger/log";
+import { createJobQueue, updateJobQueue } from "../repos/repos.queue";
+import { CloudEventV1 } from "cloudevents";
 
 function startWorker(queueName: string) {
-    // Use looger instead of console.log
-  console.log("Worker started... ");
+  logger.info("Worker started... ");
+  let jobId: string | undefined;
   const worker = new Worker(
     queueName,
     async (job: Job) => {
-      console.log(
-        `Processing job with ID: ${job.id}, mediaId: ${
-          job.data.mediaId
-        }, data: ${JSON.stringify(job.data)}`
-      );
+      logger.debug("job 1");
+      jobId = job.id;
+
+      // To test job failure
+      // throw new Error("Testing Failure error");
+      logger.info(`Processing job with ID: ${job.id} data: ${JSON.stringify(job.data)}`);
     },
     {
       connection: redisConnection,
       removeOnComplete: { count: 0 },
     }
-  );        
-
+  );
+  
   worker.on("error", (error) => {
     // UPdate JOB_QUEUE table with error along with job id
-    console.error("Worker error:", error);
+    logger.error("Worker error:", error);
+    updateJobQueue(queueName, jobId || "", { status: "Error", error: error.message });
   });
 
   worker.on("failed", (job, error) => {
     // UPdate JOB_QUEUE table with error along with job id
-    console.error(`Job $.id} failed with error: ${error.message}`);
+    updateJobQueue(queueName, job?.id || "", { status: "Failed", error: error.message, completedAt: new Date().toISOString() });
+    logger.error(`Job ${job?.id} failed with error: ${error.message}`);
   });
 
   worker.on("completed", (job) => {
     // UPdate JOB_QUEUE table with status along with job id
-    console.log(`Job ${job.id} completed successfully.`);
+    updateJobQueue(queueName, job?.id || "", { status: "Completed", completedAt: new Date().toISOString() });
+    logger.info(`Job ${job.id} completed successfully.`);
   });
+
+  // To test job execution error
+  // throw new Error("Testing worker job execution error");
 }
 
 // Come up with cloudevent as a message format
-async function addJob(queueName: string, data: any) {
-    const adBreaksQueue = new Queue("adBreaksQueue", { connection: redisConnection });
-    const result = await adBreaksQueue.add(queueName, data);
-    // Add record to table JOB_QUEUE
+async function addJob(queueName: string, data: CloudEventV1<any>) {
+  const queue = new Queue(queueName, { connection: redisConnection });
+  queue.add(queueName, data).then(async createdJob => {
+    await createJobQueue(queueName, createdJob.id || "", createdJob.data);
+  });
+  return { status: "Processing..." };
 }
+
 export { startWorker, addJob };
