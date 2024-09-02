5635c6db842f04b01d23797da5d07a84a0f1d819
Deepika Pabbisetti <deepika_pabbisetti@tecnics.com>
2024-09-02 16:13:54 +0530

    Create generateDbUrl.ts
    To generate db url from db configuration

---
+++ src/utils/generateDbUrl.ts
@@ -0,0 +1,33 @@
+import { existsSync, readFileSync, writeFileSync, appendFileSync } from 'fs';
+import { join } from 'path';
+import config from '../config';
+import { logger } from '../logger/log';
+
+const constructDatabaseUrl = (): string => {
+    const { host, user, pass, name, connectionLimit } = config.db;
+    return `postgresql://${user}:${pass}@${host}:5432/${name}?connection_limit=${connectionLimit}`;
+};
+
+const envPath = join(process.cwd(), 'src', 'prisma', '.env');
+const databaseUrlEntry = `DATABASE_URL="${constructDatabaseUrl()}"`;
+
+const generateEnvFile = () => {
+    if (existsSync(envPath)) {
+        const envContent = readFileSync(envPath, 'utf-8');
+        if (envContent.includes('DATABASE_URL=')) {
+            const updatedContent = envContent.replace(/DATABASE_URL=.*/g, databaseUrlEntry);
+            writeFileSync(envPath, updatedContent, 'utf-8');
+            logger.info('.env file updated with new DATABASE_URL.');
+        } else {
+            appendFileSync(envPath, `\n${databaseUrlEntry}`, 'utf-8');
+            logger.info('DATABASE_URL added to existing .env file.');
+        }
+    } else {
+        writeFileSync(envPath, `${databaseUrlEntry}\n`, 'utf-8');
+        logger.info('.env file created and DATABASE_URL added.');
+    }
+};
+
+generateEnvFile();
+
+export { constructDatabaseUrl, generateEnvFile };
