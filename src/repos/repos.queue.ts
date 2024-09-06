d86a04379ec496e3ec883fb2d880ba58e4082211
Deepika2430 <deepika_pabbisetti@tecnics.com>
2024-09-06 12:59:09 +0530

    Add getAllJobs() and getJobById()

--- src/repos/repos.queue.ts
+++ src/repos/repos.queue.ts
@@ -1,30 +1,88 @@
 import { CloudEvent } from "cloudevents";
 import { prismaConnection } from "../connections";
 import { JobQueue as jobQueueModel } from "@prisma/client"
+import { logger } from "../logger/log";
+import { getAllJobsRequest } from "../types/types.queue";
 
-async function createJobQueue(queueName: string, id: string, payload: CloudEvent): Promise<jobQueueModel> {
-    const insertedJob: jobQueueModel = await prismaConnection.jobQueue.create({
-        data: {
-            queueName: queueName,
-            status: "Waiting",
-            jobId: id,
-            payload: payload,
-            createdAt: payload.time
-        }
-    });
-    return insertedJob;
-}
+type Result<T> = {
+    data?: T;
+    error?: string;
+};
 
-async function updateJobQueueResults(queueName: string, id: string, updates: { status: string, completedAt?: string, error?: string }) {
-    const updatedJob = await prismaConnection.jobQueue.update({
-        where: {
-            queueName_jobId: {
+async function createJobQueue(queueName: string, id: string, payload: CloudEvent): Promise<Result<jobQueueModel>> {
+    try {
+        const insertedJob: jobQueueModel = await prismaConnection.jobQueue.create({
+            data: {
                 queueName: queueName,
-                jobId: id
+                status: "Waiting",
+                jobId: id,
+                payload: payload,
+                createdAt: payload.time
             }
-        },
-        data: updates
-    });
-    return updatedJob;
+        });
+        return { data: insertedJob };
+    } catch (error: any) {
+        logger.error(`Error creating job: ${error}`);
+        return { error: error.meta ? error.meta.cause ? error.meta.cause : error : error };
+    }
 }
-export { createJobQueue, updateJobQueueResults }
+
+async function updateJobQueueResults(queueName: string, id: string, updates: { status: string, completedAt?: string | null, error?: string | null }): Promise<Result<jobQueueModel>> {
+    try {
+        const updatedJob = await prismaConnection.jobQueue.update({
+            where: {
+                queueName_jobId: {
+                    queueName: queueName,
+                    jobId: id
+                }
+            },
+            data: updates
+        });
+        return { data: updatedJob };
+    } catch (error: any) {
+        logger.error(`Error updating jobs: ${error}`);
+        return { error: error.meta ? error.meta.cause ? error.meta.cause : error : error };
+    }
+}
+
+async function getAllJobs(req: getAllJobsRequest): Promise<Result<jobQueueModel[]>> {
+    try {
+        const { sort, filter, pagination } = req;
+        const filterKey = Object.keys(filter)[0];
+        const [sortBy, order] = sort;
+        const [page, perPage] = pagination;
+        const jobs = await prismaConnection.jobQueue.findMany({
+            where : {
+                [filterKey]: filter?.filterKey
+            },
+            orderBy: {
+                    [sortBy]: order.toLowerCase()
+                },
+                skip: page * perPage,
+                take: perPage
+        });
+        return { data: jobs };
+    } catch (error: any) {
+        logger.error(`Error fetching jobs:${error}`);
+        return { error: error.meta ? error.meta.cause ? error.meta.cause : error : error };
+    }
+}
+
+async function getJobById(id: string): Promise<Result<jobQueueModel>> {
+    try {
+        const job = await prismaConnection.jobQueue.findUnique({
+           where :{
+            id: id
+           }
+        });
+        if (!job) {
+            return {error: "Not found"}
+        }
+        return { data: job };
+    } catch (error: any) {
+        logger.error(`Error fetching jobs:${error}`);
+        return { error: error.meta ? error.meta.cause ? error.meta.cause : error : error };
+    }
+}
+
+export { createJobQueue, updateJobQueueResults, getAllJobs, getJobById }
