25e73c34e8cd452209c6852f55248e29c8d0e8fa
Deepika2430 <deepika_pabbisetti@tecnics.com>
2024-09-18 17:08:41 +0530

    Change authentication to auth0

---
+++ src/middleware/auth-middleware.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+
+import config from '../config';
+
+const domain = config.swagger.domain;
+console.log(domain);
+
+export const redirectToAuthorizationUrl = (req: Request, res: Response) => {
+  const responseType = req.query.response_type;
+  const clientId = req.query.client_id;
+  const redirectUri = req.query.redirect_uri;
+  const state = req.query.state; // For further usage
+  const audience = `https://${domain}/api/v2/`;
+
+  const authorizationUrl = `https://${domain}/authorize?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirectUri}&audience=${audience}&state=${state}`;
+  res.redirect(authorizationUrl);
+};
--- src/middleware/jwt-authorization.ts
+++ src/middleware/jwt-authorization.ts
@@ -1,8 +1,24 @@
-import { expressjwt } from 'express-jwt';
+import https from 'https';
 
-const jwtSecret = 'jwt-secret';
+import { expressjwt, GetVerificationKey } from 'express-jwt';
+import jwksRsa from 'jwks-rsa';
 
-export const jwtMiddleware = expressjwt({
-  secret: jwtSecret,
-  algorithms: ['HS256'],
+import config from '../config';
+
+const domain = `https://dev-fulrxmb8tfh5ke8o.us.auth0.com${config.swagger.domain}`;
+
+const agent = new https.Agent({
+  rejectUnauthorized: false,
 });
+
+const jwtAuthMiddleware = expressjwt({
+  secret: jwksRsa.expressJwtSecret({
+    jwksUri: `${domain}/.well-known/jwks.json`,
+    requestAgent: agent,
+  }) as GetVerificationKey,
+  audience: `${domain}/api/v2/`,
+  issuer: `${domain}/`,
+  algorithms: ['RS256'],
+});
+
+export default jwtAuthMiddleware;
