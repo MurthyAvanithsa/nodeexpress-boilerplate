e13c5b9c96ee7649753dd5d6e9081dbbffd7bca5
Rajesh.K <rajesh_kandregula@tecnics.com>
2024-09-23 17:51:38 +0530

    Removed logger statement

--- src/utils/decodeToken.ts
+++ src/utils/decodeToken.ts
@@ -1,13 +1,10 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
-import { logger } from '../logger/log';
-
 export interface payload extends JwtPayload {
     "http://group"?: string;
 }
 
 export const getDecodedToken = (authToken: string) => {
-    logger.info(authToken);
     const token = authToken.split(' ')[1];
     return jwt.decode(token) as payload;
 };
