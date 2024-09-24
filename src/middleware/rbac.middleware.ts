c38f9c009ac59130cbfc7c94dae87bc43891e27f
Deepika2430 <deepika_pabbisetti@tecnics.com>
2024-09-24 16:40:14 +0530

    Update groupRoleMapping

--- src/middleware/rbac.middleware.ts
+++ src/middleware/rbac.middleware.ts
@@ -1,106 +1,62 @@
 import { Request, NextFunction, Response } from 'express';
 
-import { getDecodedToken, payload } from "../utils/decodeToken";
+import { logger } from '../logger/log';
 
-type Permission = {
-    method: string,
-    path: string,
-};
+export interface customRequest extends Request {
+    auth :{
+        [key: string]: string
+    }
+}
+
 
 type GroupRoleMapping = {
-    group: string,
-    role: string,
-    status: boolean,
-    permissions: Permission[],
-};
-
-// type Group = {
-//     name: string,
-//     status: boolean
-//     role: {
-//         name: string,
-//         permissions: Permission[]
-//     }[]
-// };
-
-const groupRoleMapping: GroupRoleMapping[] = [{
-    group: "grp_viewer",
-    role: "viewer",
-    status: true,
-    permissions: [{
-        method: "GET",
-        path: "/feed",
-    },
-    {
-        method: "GET",
-        path: "/feed/:id",
-    }],
-},
-{
-    group: "grp_editor",
-    role: "editor",
-    status: true,
-    permissions: [{
-        method: "GET",
-        path: "/feed",
-    },
-    {
-        method: "GET",
-        path: "/feed/:id",
-    },
-    {
-        method: "POST",
-        path: "/feed",
-    },
-    {
-        method: "PUT",
-        path: "/feed",
-    },
-    {
-        method: "DELETE",
-        path: "/feed/:id",
-    },
-    {
-        method: "GET",
-        path: "/filter",
-    },
-    {
-        method: "GET",
-        path: "/filter/:id",
-    }, {
-        method: "POST",
-        path: "/filter",
+    [key: string]: {
+        role: string,
+        [key: string]: boolean | string
     }
-    ],
-},
-{
-    group: "grp_admin",
-    role: "admin",
-    status: true,
-    permissions: [{
-        method: "*",
-        path: "*",
-    }],
-}];
-
+}
+const groupRoleMapping: GroupRoleMapping = {
+    grp_viewer: {
+        role: "viewer",
+        "GET_/feed": true,
+        "GET_/feed:id": true,
+        "GET_/filter": true,
+        "GET_/filter:id": true,
+    },
+    grp_editor: {
+        role: "editor",
+        "GET_/feed": true,
+        "GET_/feed:id": true,
+        "POST_/feed": true,
+        "PUT_/feed:id": true,
+        "GET_/filter": true,
+        "GET_/filter:id": true,
+        "PUT_/filter:id": true,
+    },
+    grp_admin: {
+        role: "admin",
+        "*_*": true
+    }
+}
 export const checkRolesAndPermissions = (req: Request, res: Response, next: NextFunction) => {
-    const decodeToken: payload = req.headers.authorization ? getDecodedToken(req.headers.authorization): {};
-    const role = decodeToken?.["http://group"];
+    const group = (req as customRequest).auth?.["http://group"];
+    logger.info(group);
     const method = req.method;
     const path = req.path;
-    const rolePermissions = groupRoleMapping.find(groupRole => groupRole.role === role && groupRole.status);
-    if (!rolePermissions) {
+
+    const groupPermissions: {
+        role: string,
+        [key: string]: boolean | string
+    } = groupRoleMapping[group];
+    if (!groupPermissions) {
         return res.status(403).json({ message: 'Forbidden - You do not have the necessary role' });
-    }
-
-    const hasPermission = rolePermissions.permissions.some(permission =>
-        (permission.method === method || permission.method === '*') &&
-        (permission.path === path || permission.path === '*')
-    );
-
-    if (hasPermission) {
+    } else if (groupPermissions["*_*"] === true) {
         return next();
     } else {
+        const pathPermissions: string | boolean = groupPermissions[`${method}_${path}`];
+        if (pathPermissions === true) {
+            return next();
+        }
         return res.status(403).json({ message: 'Forbidden - You do not have the necessary permissions' });
     }
 };
