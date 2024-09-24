fb233117b6d86f447240ee920109377aa725c86c
Deepika2430 <deepika_pabbisetti@tecnics.com>
2024-09-24 16:37:51 +0530

    Add function to register all the routes

--- src/utils/registerRoutes.ts
+++ src/utils/registerRoutes.ts
@@ -1,14 +1,31 @@
-import { logger } from "../logger/log";
-export async function registerRoute(path: string): Promise<any> {
-    try {
-      const route = await import(path);
-      if (route && route.default && typeof route.default === 'function') {
-        return route.default;
-      } else {
-        throw new Error(`File with path ${path} does not export a default function`);
-      }
-    } catch (error: unknown) {
-      logger.error(`Error registering route: ${(error as unknown as any)?.message}`);
-      return null;
+// src/routes/index.ts
+import path from 'path';
+import fs from 'fs';
+
+import { Router } from 'express';
+
+import { logger } from '../logger/log';
+
+const router = Router();
+
+const routePath = path.join(__dirname, '../routes');
+const routeFiles = fs.readdirSync(routePath).filter(file => file.startsWith('routes.') && file.endsWith('.ts'));
+
+(async () => {
+    for (const file of routeFiles) {
+        const filePath = path.join(routePath, file);
+
+        try {
+            const route = await import(filePath);
+            if (route?.default && typeof route.default === 'function') {
+                router.use(route.default);
+            } else {
+                logger.error(`File at ${filePath} does not export a valid default function.`);
+            }
+        } catch (error) {
+            logger.error(`Error registering route from ${filePath}: ${error instanceof Error ? error.message : error}`);
+        }
     }
-}
+})();
+
+export default router;
