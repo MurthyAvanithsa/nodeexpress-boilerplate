9e163faf8592992fa8fdebc736abb999928071bb
Rajesh.K <rajesh_kandregula@tecnics.com>
2024-09-24 16:17:33 +0530

    Modify oidc endpoints

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -2,16 +2,13 @@ import { Request, Response } from 'express';
 
 import config from '../config';
 
-const domain = config.swagger.domain;
-
 export const redirectToAuthorizationUrl = (req: Request, res: Response) => {
   const responseType = req.query.response_type;
-  const clientId = req.query.client_id;
-  const redirectUri = req.query.redirect_uri;
-  const state = req.query.state; // For further usage
-  const audience = `https://${domain}/api/v2/`;
+  const clientId = config.oidc.clientId;
+  const redirectUri = config.oidc.redirectUri;
+  const state = req.query.state;
+  const audience = config.oidc.audience;
 
-  const authorizationUrl = `https://${domain}/authorize?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirectUri}&audience=${audience}&state=${state}`;
+  const authorizationUrl = `https://${config.oidc.domain}/authorize?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirectUri}&audience=${audience}&state=${state}`;
   res.redirect(authorizationUrl);
 };
-
